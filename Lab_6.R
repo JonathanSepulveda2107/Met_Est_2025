287035786e51393362b499d29278d4fcd26f7931
JonathanSepulveda2107 <joalsefl@gmail.com>
2025-11-12 08:29:31 -0600

    laboratorios

---
+++ Lab_6.R
@@ -0,0 +1,31 @@
+# Correlación de Pearson
+# Datos del geyser Old Faithful
+# 24/09/2025
+# Jonathan Alfredo Sepulveda Flores
+
+data("faithful")
+
+plot(faithful$waiting, faithful$eruptions,
+     xlab = "Tiempo de espera (min)",
+     ylab = "Erupcion",
+     col = "green",
+     pch = 20)
+
+# Correlacionar las dos variables
+
+shapiro.test(faithful$eruptions)
+shapiro.test(faithful$waiting)
+
+# Pearson solo se utiliza cuando tenemos datos normales
+#======================================================
+cor.test(faithful$waiting, faithful$eruptions,
+         method = "pearson")
+
+
+
+#======================================================
+# Sperman se utiliza como contraparte para datos no normales
+#======================================================
+
+cor.test(faithful$waiting, faithful$eruptions,
+         method = "spearman")
